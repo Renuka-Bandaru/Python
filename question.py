df3b776a1f5e233a5d9c814d4501b99ee48d4a21
Renuka-Bandaru <renuka.bandaru777@gmail.com>
2025-07-02 16:20:48 +0530

    practicing python questions

---
+++ question.py
@@ -0,0 +1,7 @@
+#You are given a list of intervals, where each interval is represented by a pair of start and end times.
+#  Your task is to merge overlapping intervals. Write a function merge_intervals(intervals)
+#  that takes a list of intervals as input and returns a new list of merged intervals.
+# [(1,3),(2,6),(17,20),(8,10),(15,18)] --- input
+# [(1,6),(8,10),(15,20)] ----- output
+
+print("hellow world")

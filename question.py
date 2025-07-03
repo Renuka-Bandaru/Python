8a2d912cb32219f024fa0120d984d0334a29a438
Renuka-Bandaru <renuka.bandaru777@gmail.com>
2025-07-03 16:39:53 +0530

    merged intervals question solved

--- question.py
+++ question.py
@@ -4,4 +4,27 @@
 # [(1,3),(2,6),(17,20),(8,10),(15,18)] --- input
 # [(1,6),(8,10),(15,20)] ----- output
 
-print("hellow world")
+def mergeInterval(intervals):
+    intervals.sort()
+    print(intervals)
+    current_interval = intervals[0]
+    print(current_interval)
+    result = []
+    for rem_intervals in intervals[1:]:
+        if (rem_intervals[0] <= current_interval[1]):
+                current_interval = (current_interval[0], max(current_interval[1], rem_intervals[1]))
+                print(current_interval,"current_interval")
+        else:
+             result.append(current_interval)
+             current_interval = rem_intervals
+
+    result.append(current_interval)
+    return result
+
+
+
+
+intervals = [(1,3), (2,6),(17,20),(8,10), (15,18)]
+merged_interval = mergeInterval(intervals)
+
+print(merged_interval,"merged_intervals")
